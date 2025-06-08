68e783f6543204d563e50fabfc378bb4ea1b1e58
Hail <the.crazycat@outlook.com>
2025-06-08 09:56:58 +0100

    Initial commit

---
+++ hailhurl.py
@@ -0,0 +1,16 @@
+import sys
+
+
+def main(package_name):
+    pass
+
+def regularinstall(package_name):
+    pass
+
+def fastinstall(package_name):
+    pass    
+ 
+if len(sys.argv) > 1:
+    print(f"Hello, {sys.argv[1]}!")
+else:
+    print("Hello, world!")
