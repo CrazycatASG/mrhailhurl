12b9b3bcf88d7ba8554a52db2bcf15a020ac1dfb
Hail <the.crazycat@outlook.com>
2025-06-09 15:00:12 +0100

    Added argument parsing + changed functions around

--- hailhurl.py
+++ hailhurl.py
@@ -1,16 +1,13 @@
 import sys
-
-
-def main(package_name):
-    pass
+import argparse
 
 def regularinstall(package_name):
     pass
 
-def fastinstall(package_name):
+def gitcloneinstall(package_name):
     pass    
  
-if len(sys.argv) > 1:
-    print(f"Hello, {sys.argv[1]}!")
-else:
-    print("Hello, world!")
+ 
+parser = argparse.ArgumentParser()
+parser.add_argument("package_name", help="Name of the package to install")
+args = parser.parse_args()
